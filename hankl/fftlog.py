af93e5621a111e5e7a668400250f8f03ce127dab
Minas Karamanis <minaskar@gmail.com>
2020-07-01 12:45:47 +0100

    improvements

--- hankl/fftlog.py
+++ hankl/fftlog.py
@@ -1,25 +1,7 @@
-r''' This version of the fast Hankle transform is due to 
-    Andrew Hamilton (see http://casa.colorado.edu/~ajsh/FFTLog/). 
-    
-	Joseph E. McEwen 
-	McEwen Laboratories (c) 2016 
-	email: jmcewen314@gmail.com
-		
-	This code is available for anyone to use, but please give approriate reference to 
-	Joseph E. McEwen and the authors of the algorithm. 
-	
-	The Hankel transform in this code is defined as : 
-	F(k)= \int_0^\infty f(r) (kr)^q J_\mu(kr) k dr 
-	f(r)= \int_0^\infty F(k) (kr)^{-q} J_\mu(kr) r dk .  
-		
-'''
-
-from __future__ import division
-
 import numpy as np
 from numpy.fft import fft, ifft , fftshift, ifftshift , rfft, irfft
 from numpy import exp, log, log10, cos, sin, pi
-from scipy.special import gamma
+from scipy.special import loggamma, gamma
 from time import time
 from numpy import gradient as grad
 import sys
@@ -29,11 +11,6 @@ cut = 200
 
 
 def g_m_vals(mu, q):
-	"""
-	This function is copied from gamma_funcs.py in fastpt.
-	We repeat it so that HT.py is self-contained, requiring only
-	numpy and scipy.
-	"""
 	imag_q = np.imag(q)
 
 	g_m = np.zeros(q.size, dtype=complex)
@@ -58,9 +35,24 @@ def g_m_vals(mu, q):
 	return g_m
 
 
-def get_k0(N, mu, q, r0, L, k0):
+def _lowring_kr(mu, q, L, N, kr=1.0):
+	r'''
+	Compute kr so that 
 
-	kr = float(k0*r0)
+		(k r)^{- i pi/dlnr} U_{\mu}(q + i pi/dlnr)
+
+	is real thus reducing lowringing of the Hankel Transform.
+
+	Args:
+		mu (float): Index of J_mu in Hankel transform; mu may be any real number, positive or negative.
+		q (float): Exponent of power law bias; q may be any real number, positive or negative.
+		L (float): Range of uniformly logarithmically spaced points.
+		N (int): Number of uniformly logarithmically spaced points.
+		kr (float): Input value of kr (Default is 1).
+	Returns:
+		kr (float): Low-ringing value of kr nearest to input kr.
+	'''
+
 	delta_L = L/float(N)
 
 	x = q + 1j*pi/delta_L
@@ -68,10 +60,10 @@ def get_k0(N, mu, q, r0, L, k0):
 	x_plus = (mu+1+x)/2.
 	x_minus = (mu+1-x)/2.
 
-	phip = np.imag(log(gamma(x_plus)))
-	phim = np.imag(log(gamma(x_minus)))
+	phip = np.imag(loggamma(x_plus))
+	phim = np.imag(loggamma(x_minus))
 
-	arg = log(2/kr)/delta_L + (phip - phim)/pi
+	arg = log(2.0/kr)/delta_L + (phip - phim)/pi
 	iarg = np.rint(arg)
 	if (arg != iarg):
 		kr = kr*exp((arg-iarg)*delta_L)
@@ -94,7 +86,7 @@ def u_m_vals(m, mu, q, kr, L):
     return u_m
 
 
-def FFTLog(k, f_k, q, mu, lowring=False):
+def FFTLog(k, f_k, q, mu, kr=1.0, lowring=False):
 	r'''
 	Hankel Transform based on the FFTLog algorithm of [1] and [2].
 
@@ -112,48 +104,30 @@ def FFTLog(k, f_k, q, mu, lowring=False):
 	Args:
 		k (array):
 		f_k (array):
-		q (float):
-		mu (float):
-		lowring (bool):
+		q (float): Exponent of power law bias; q may be any real number, positive or negative.
+		mu (float): Index of J_mu in Hankel transform; mu may be any real number, positive or negative.
+		kr (float): Input value of kr (Default is 1).
+		lowring (bool): If True, then use low-ringing value of kr closest to input value of kr.
 	Returns:
-		Two arrays
+		r (array):
+		A (array):
 	'''
-
-	if ((q+mu) < -1):
-		print('Error in reality condition for Bessel function integration.')
-		print(' q+mu is less than -1.')
-		print('See Abramowitz and Stegun. Handbook of Mathematical Functions pg. 486')
-
-	if (q > 1/2.):
-		print('Error in reality condition for Bessel function integration.')
-		print(' q is greater than 1/2')
-		print('See Abramowitz and Stegun. Handbook of Mathematical Functions pg. 486')
 
 	N = f_k.size
 	delta_L = (log(np.max(k))-log(np.min(k)))/float(N-1)
 	L = (log(np.max(k))-log(np.min(k)))
 
-	# find a better way to check if it is evenly spaced in log
-	diff = np.diff(np.log(k))
-	diff = np.diff(diff)
-	if (np.sum(diff) >= 1e-10):
-		print('You need to send in data that is sampled evenly in logspace')
-		print('Terminating code in fft_log')
-		sys.exit()
 
 	log_k0 = log(k[N//2])
 	k0 = exp(log_k0)
 
-	# Fourier transform input data
-	# get m values, shifted so the zero point is at the center
+	# Fourier transform input data and get m values, shifted so the zero point is at the center
 
 	c_m = rfft(f_k)
 	m = np.fft.rfftfreq(N, d=1.)*float(N)
 	# make r vector
 	if lowring:
-		kr=get_k0(float(N),mu,q,1/k0,L,k0)
-	else:
-		kr = 1
+		kr = _lowring_kr(mu, q, L, N, kr)
 	r0 = kr/k0
 	log_r0 = log(r0)
 
@@ -165,8 +139,6 @@ def FFTLog(k, f_k, q, mu, lowring=False):
 	s = delta_L*(-m_r)+log_r0
 	id = m_shift
 	r = 10**(s[id]/log(10))
-
-	#m_shift=np.fft.fftshift(m)
 
 	u_m = u_m_vals(m, mu, q, kr, L)
 
