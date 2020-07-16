27c365c40d45755f9eb5c7df8f686b45e69ba51c
Minas Karamanis <minaskar@gmail.com>
2020-07-16 15:53:42 +0100

    replaced kr with xy

--- hankl/fftlog.py
+++ hankl/fftlog.py
@@ -45,11 +45,11 @@ def _gamma_term(mu, x, cutoff=200.0):
 	return g_m
 
 
-def _lowring_kr(mu, q, L, N, kr=1.0):
+def _lowring_xy(mu, q, L, N, xy=1.0):
 	r'''
-	Compute kr so that 
+	Compute xy so that 
 
-		(k r)^{- i \pi/dlnr} U_{\mu}(q + i \pi/dlnr)
+		(x y)^{- i \pi/dlnr} U_{\mu}(q + i \pi/dlnr)
 
 	is real thus reducing lowringing of the Hankel Transform.
 
@@ -58,9 +58,9 @@ def _lowring_kr(mu, q, L, N, kr=1.0):
 		q (float): Exponent of power law bias; q may be any real number, positive or negative.
 		L (float): Range of uniformly logarithmically spaced points.
 		N (int): Number of uniformly logarithmically spaced points.
-		kr (float): Input value of kr (Default is 1).
+		xy (float): Input value of xy (Default is 1).
 	Returns:
-		kr (float): Low-ringing value of kr nearest to input kr.
+		xy (float): Low-ringing value of xy nearest to input xy.
 	'''
 
 	delta_L = L/float(N)
@@ -73,19 +73,19 @@ def _lowring_kr(mu, q, L, N, kr=1.0):
 	phip = np.imag(np.log(gamma(x_plus)))
 	phim = np.imag(np.log(gamma(x_minus)))
 
-	arg = np.log(2.0/kr)/delta_L + (phip - phim)/np.pi
+	arg = np.log(2.0/xy)/delta_L + (phip - phim)/np.pi
 	iarg = np.rint(arg)
 	if (arg != iarg):
-		kr = kr*np.exp((arg-iarg)*delta_L)
+		xy = xy*np.exp((arg-iarg)*delta_L)
 
-	return kr
+	return xy
 
 
-def _u_m_term(m, mu, q, kr, L):
+def _u_m_term(m, mu, q, xy, L):
 	r'''
 	Compute u_{m}(\mu, q) term defined as
 
-		u_{m}(\mu, q) = (k_{0}r_{0})^{-2\pi i m / L} U_{\mu}(q + 2\pi i m/ L)
+		u_{m}(\mu, q) = (x_{0}y_{0})^{-2\pi i m / L} U_{\mu}(q + 2\pi i m/ L)
 
 	(see eq.18 in https://jila.colorado.edu/~ajsh/FFTLog/)
 
@@ -93,7 +93,7 @@ def _u_m_term(m, mu, q, kr, L):
 		m (float): Index of u_{m} term.
 		mu (float): Index of J_mu in Hankel transform; mu may be any real number, positive or negative.
 		q (float):  Exponent of power law bias; q may be any real number, positive or negative.
-		kr (float): Value of kr.
+		xy (float): Value of xy.
 		L (float): Range of uniformly logarithmically spaced points.
 	Returns:
 		u_{m}(\mu, q) (float) : u_{m} term of eq. 18
@@ -105,33 +105,31 @@ def _u_m_term(m, mu, q, kr, L):
 
 	U_mu = 2**x*_gamma_term(mu, x)
 
-	u_m = (kr)**(-omega)*U_mu
+	u_m = (xy)**(-omega)*U_mu
 
 	u_m[m.size-1] = np.real(u_m[m.size-1])
 
 	return u_m
 
 
-def FFTLog(k, f_k, q, mu, kr=1.0, lowring=False):
+def FFTLog(x, f_x, q, mu, xy=1.0, lowring=False):
 	r'''
 	Hankel Transform based on the FFTLog algorithm of [1] and [2].
 
 	Defined as:
 
-	.. math:: F(k)= \int_0^\infty f(r) (kr)^q J_\mu(kr) k dr 
-
-	.. math:: f(r)= \int_0^\infty F(k) (kr)^{-q} J_\mu(kr) r dk
+	.. math:: f(y)= \int_0^\infty F(x) (xy)^{q} J_\mu(xy) y dx
 
 	Args:
-		k (array): Array of uniformly logarithmically spaced k (or r) values.
-		f_k (array): Array of respective F(k) (or f(r)) values.
+		x (array): Array of uniformly logarithmically spaced x values.
+		f_x (array): Array of respective F(x) values.
 		q (float): Exponent of power law bias; q may be any real number, positive (for forward transform) or negative (for inverse transform).
 		mu (float): Index of J_mu in Hankel transform; mu may be any real number, positive or negative.
-		kr (float): Input value of kr (Default is 1).
-		lowring (bool): If True, then use low-ringing value of kr closest to input value of kr (Default is False).
+		xy (float): Input value of xy (Default is 1).
+		lowring (bool): If True, then use low-ringing value of xy closest to input value of xy (Default is False).
 	Returns:
-		r (array): Array of uniformly logarithmically spaced r (or k) values.
-		A (array): Array of respective f(r) (or F(k)) values.
+		y (array): Array of uniformly logarithmically spaced y values.
+		f_y (array): Array of respective f(y) values.
 
 	References:
 		[1] J. D. Talman. Numerical Fourier and Bessel Transforms in Logarithmic Variables. Journal of Computational Physics, 29:35-48, October 1978.
@@ -142,38 +140,38 @@ def FFTLog(k, f_k, q, mu, kr=1.0, lowring=False):
 	if mu + 1.0 + q == 0.0:
 		raise ValueError('The FFTLog Hankel Transform is singular when mu + 1 + q = 0.')
 
-	N = f_k.size
-	delta_L = (np.log(np.max(k))-np.log(np.min(k)))/float(N-1)
-	L = np.log(np.max(k)) - np.log(np.min(k))
-	log_k0 = np.log(k[N//2])
-	k0 = np.exp(log_k0)
+	N = f_x.size
+	delta_L = (np.log(np.max(x))-np.log(np.min(x)))/float(N-1)
+	L = np.log(np.max(x)) - np.log(np.min(x))
+	log_x0 = np.log(x[N//2])
+	x0 = np.exp(log_x0)
 
-	c_m = np.fft.rfft(f_k)
+	c_m = np.fft.rfft(f_x)
 	m = np.fft.rfftfreq(N, d=1.)*float(N)
 	if lowring:
-		kr = _lowring_kr(mu, q, L, N, kr)
-	r0 = kr/k0
-	log_r0 = np.log(r0)
+		xy = _lowring_xy(mu, q, L, N, xy)
+	y0 = xy/x0
+	log_y0 = np.log(y0)
 
-	m_r = np.arange(-N//2, N//2)
-	m_shift = np.fft.fftshift(m_r)
+	m_y = np.arange(-N//2, N//2)
+	m_shift = np.fft.fftshift(m_y)
 
-	s = delta_L*(-m_r)+log_r0
+	s = delta_L*(-m_y)+log_y0
 	id = m_shift
-	r = 10**(s[id]/np.log(10))
+	y = 10**(s[id]/np.log(10))
 
-	u_m = _u_m_term(m, mu, q, kr, L)
+	u_m = _u_m_term(m, mu, q, xy, L)
 	b = c_m*u_m
 
 	A_m = np.fft.irfft(b)
-	A = A_m[id]
+	f_y = A_m[id]
 
-	A = A[::-1]
-	r = r[::-1]
+	f_y = f_y[::-1]
+	y = y[::-1]
 
 	if (q != 0):
-		A = A*(r)**(-float(q))
+		f_y = f_y*(y)**(-float(q))
 
-	return r, A
+	return y, f_y
 
 
