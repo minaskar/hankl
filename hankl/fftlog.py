e478bd27d8abbee05bf953b17153bd1120ce17bc
Minas Karamanis <minaskar@gmail.com>
2020-07-16 18:15:35 +0100

    docs

--- hankl/fftlog.py
+++ hankl/fftlog.py
@@ -136,14 +136,16 @@ def FFTLog(x, f_x, q, mu, xy=1.0, lowring=False, ext=0, range=None, return_ext=F
 		ext (int or tuple or list): Controls the extrapolation mode. When ext is an integer then the same extrapolation method will
             be used for both ends of the input array. Alternatively, when ext is an tuple (ext_left, ext_right) or a list [ext_left,
             ext_right] then different methods can be used for the two ends of the the input array.
+
             * if ext=0 then no extrapolation is performed (Default).
             * if ext=1 then zero padding is performed.
             * if ext=2 then constant padding is performed.
             * if ext=3 then Power-Law extrapolation is performed.
+			
         range (tuple or list): The minimum extrapolation range in the form of a tuple (x_min, x_max) or list [x_min, x_max]. When
             range=None (Default) then the extended range is chosen automatically such that its array-size is the next power of two.
 		return_ext (bool): When False (Default) the result is cropped to fit the original x range.
-		
+
 	Returns:
 		y (array): Array of uniformly logarithmically spaced y values.
 		f_y (array): Array of respective f(y) values.
